663237aaca9010c4ad64f1fbb6c3137af8e2f7ae
Artem Chetverikov <av.chetverikov@tensor.ru>
2024-12-03 22:59:38 +0500

    *

--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -4,11 +4,11 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class platform(TemplateView):
-    template_name = 'third_task/platform.html'
+    template_name = 'fourth_task/platform.html'
 
 
 class games(TemplateView):
-    template_name = 'third_task/games.html'
+    template_name = 'fourth_task/games.html'
     games = ['Atomic Heart', 'Cyberpunk 2077', 'PayDay 2']
     extra_context = {
         'games': games
@@ -16,5 +16,5 @@ class games(TemplateView):
 
 
 class cart(TemplateView):
-    template_name = 'third_task/cart.html'
+    template_name = 'fourth_task/cart.html'
 
